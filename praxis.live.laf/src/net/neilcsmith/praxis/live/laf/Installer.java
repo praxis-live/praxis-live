133b136acdfd35c193c485f5fde6e3f1e8cf3f43
Neil C Smith <neilcsmith.net@googlemail.com>
2011-02-20 18:26:13 +0000

    First commit - LAF installer module.

---
+++ praxis.live.laf/src/net/neilcsmith/praxis/live/laf/Installer.java
@@ -0,0 +1,91 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 2010 Neil C Smith.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details.
+ *
+ * You should have received a copy of the GNU General Public License version 2
+ * along with this work; if not, see http://www.gnu.org/licenses/
+ *
+ *
+ * Linking this work statically or dynamically with other modules is making a
+ * combined work based on this work. Thus, the terms and conditions of the GNU
+ * General Public License cover the whole combination.
+ *
+ * As a special exception, the copyright holders of this work give you permission
+ * to link this work with independent modules to produce an executable,
+ * regardless of the license terms of these independent modules, and to copy and
+ * distribute the resulting executable under terms of your choice, provided that
+ * you also meet, for each linked independent module, the terms and conditions of
+ * the license of that module. An independent module is a module which is not
+ * derived from or based on this work. If you modify this work, you may extend
+ * this exception to your version of the work, but you are not obligated to do so.
+ * If you do not wish to do so, delete this exception statement from your version.
+ *
+ * Please visit http://neilcsmith.net if you need additional information or
+ * have any questions.
+ */
+package net.neilcsmith.praxis.live.laf;
+
+import java.awt.EventQueue;
+import java.lang.reflect.InvocationTargetException;
+import javax.swing.LookAndFeel;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import net.neilcsmith.praxis.laf.PraxisLookAndFeel;
+import org.openide.modules.ModuleInstall;
+import org.openide.util.Exceptions;
+import org.openide.util.Lookup;
+
+/**
+ * Manages a module's lifecycle. Remember that an installer is optional and
+ * often not needed at all.
+ */
+public class Installer extends ModuleInstall {
+
+    @Override
+    public void restored() {
+        try {
+            EventQueue.invokeAndWait(new Runnable() {
+
+                @Override
+                public void run() {
+                    ClassLoader cl = Lookup.getDefault().lookup(ClassLoader.class);
+                    UIManager.put("ClassLoader", cl);
+
+                    try {
+                        try {
+                            UIManager.put("Nb.PraxisLFCustoms", Class.forName("org.netbeans.swing.plaf.metal.MetalLFCustoms", true, cl).newInstance());
+                        } catch (ClassNotFoundException ex) {
+                            Exceptions.printStackTrace(ex);
+                        }
+                    } catch (InstantiationException ex) {
+                        Exceptions.printStackTrace(ex);
+                    } catch (IllegalAccessException ex) {
+                        Exceptions.printStackTrace(ex);
+                    }
+
+                    try {
+                        LookAndFeel laf = new PraxisLookAndFeel();
+                        UIManager.setLookAndFeel(laf);
+                    } catch (UnsupportedLookAndFeelException ex) {
+                        Exceptions.printStackTrace(ex);
+                    }
+
+                }
+            });
+        } catch (InterruptedException ex) {
+            Exceptions.printStackTrace(ex);
+        } catch (InvocationTargetException ex) {
+            Exceptions.printStackTrace(ex);
+        }
+    }
+}
