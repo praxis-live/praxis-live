83d71ccef1d4627281b4488bae19dae7a9a3aa47
Neil C Smith <neilcsmith.net@googlemail.com>
2011-06-21 17:33:12 +0100

    Set netbeans.buildnumber system property in Installer, primarily to fix About dialog.

--- praxis.live.core/src/net/neilcsmith/praxis/live/core/Installer.java
+++ praxis.live.core/src/net/neilcsmith/praxis/live/core/Installer.java
@@ -6,8 +6,10 @@ package net.neilcsmith.praxis.live.core;
 
 import net.neilcsmith.praxis.live.core.api.HubManager;
 import net.neilcsmith.praxis.live.core.api.HubStateException;
+import org.openide.modules.ModuleInfo;
 import org.openide.modules.ModuleInstall;
 import org.openide.util.Exceptions;
+import org.openide.util.Lookup;
 
 /**
  * Manages a module's lifecycle. Remember that an installer is optional and
@@ -17,6 +19,17 @@ public class Installer extends ModuleInstall {
 
     @Override
     public void restored() {
+
+        String version = "EA:";
+
+        for (ModuleInfo info : Lookup.getDefault().lookupAll(ModuleInfo.class)) {
+            if (info.owns(this.getClass())) {
+                version = version + info.getImplementationVersion();
+            }
+        }
+        System.setProperty("netbeans.buildnumber", version);
+
+
         try {
             HubManager.getDefault().start();
         } catch (HubStateException ex) {
