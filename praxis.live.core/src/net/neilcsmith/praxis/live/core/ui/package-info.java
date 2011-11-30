7e5eff24b3cfa2fcf22a2663bf62a261e384b9cd
Neil C Smith <neilcsmith.net@googlemail.com>
2011-11-30 12:21:06 +0000

    Core primary options panel. Layer fixes - incl. temporary removal of Plugins from menu.

---
+++ praxis.live.core/src/net/neilcsmith/praxis/live/core/ui/package-info.java
@@ -0,0 +1,10 @@
+
+@ContainerRegistration(id = "Core",
+        categoryName = "#OptionsCategory_Name_Core",
+        iconBase = "net/neilcsmith/praxis/live/core/resources/core32.png",
+        keywords = "#OptionsCategory_Keywords_Core",
+        keywordsCategory = "Core",
+        position = -100)
+package net.neilcsmith.praxis.live.core.ui;
+
+import org.netbeans.spi.options.OptionsPanelController.ContainerRegistration;
