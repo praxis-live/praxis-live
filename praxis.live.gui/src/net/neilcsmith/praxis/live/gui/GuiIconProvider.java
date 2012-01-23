e7f215ce1fbdfb0af02b72e65368e884c307000c
Neil C Smith <neilcsmith.net@googlemail.com>
2012-01-23 20:09:38 +0000

    GUI support module. GUI component icon provider.

---
+++ praxis.live.gui/src/net/neilcsmith/praxis/live/gui/GuiIconProvider.java
@@ -0,0 +1,48 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 2012 Neil C Smith.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 3 for more details.
+ *
+ * You should have received a copy of the GNU General Public License version 3
+ * along with this work; if not, see http://www.gnu.org/licenses/
+ *
+ *
+ * Please visit http://neilcsmith.net if you need additional information or
+ * have any questions.
+ */
+package net.neilcsmith.praxis.live.gui;
+
+import java.awt.Image;
+import net.neilcsmith.praxis.core.ComponentType;
+import net.neilcsmith.praxis.live.components.api.ComponentIconProvider;
+import org.openide.util.ImageUtilities;
+import org.openide.util.lookup.ServiceProvider;
+
+/**
+ *
+ * @author Neil C Smith <http://neilcsmith.net>
+ */
+@ServiceProvider(service = ComponentIconProvider.class)
+public class GuiIconProvider implements ComponentIconProvider {
+
+    private final static Image AUDIO_ICON = ImageUtilities.loadImage(
+            "net/neilcsmith/praxis/live/gui/resources/gui.png", true);
+
+    @Override
+    public Image getIcon(ComponentType type) {
+        if ("root:gui".equals(type.toString())
+                || type.toString().startsWith("gui:")) {
+            return AUDIO_ICON;
+        }
+        return null;
+    }
+}
