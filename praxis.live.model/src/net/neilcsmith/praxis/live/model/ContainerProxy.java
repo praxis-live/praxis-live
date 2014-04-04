76077c324fbe2c848bbea4fb4812cc6ebfe410e5
Neil C Smith <neilcsmith.net@googlemail.com>
2014-04-04 19:43:47 +0100

    praxis.live.model - start of general API for modelling praxis components and graphs. Derived from code in praxis.live.pxr

---
+++ praxis.live.model/src/net/neilcsmith/praxis/live/model/ContainerProxy.java
@@ -0,0 +1,46 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 2014 Neil C Smith.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 3 for more details.
+ *
+ * You should have received a copy of the GNU General Public License version 3
+ * along with this work; if not, see http://www.gnu.org/licenses/
+ *
+ *
+ * Please visit http://neilcsmith.net if you need additional information or
+ * have any questions.
+ */
+package net.neilcsmith.praxis.live.model;
+
+import net.neilcsmith.praxis.live.core.api.Callback;
+import net.neilcsmith.praxis.core.ComponentType;
+
+/**
+ *
+ * @author Neil C Smith (http://neilcsmith.net)
+ */
+public interface ContainerProxy {
+
+    public void addChild(String id, ComponentType type, Callback callback)
+            throws ProxyException;
+
+    public void removeChild(String id, Callback callback) throws ProxyException;
+
+    public String[] getChildIDs();
+
+    public void connect(Connection connection, Callback callback) throws ProxyException;
+
+    public void disconnect(Connection connection, Callback callback) throws ProxyException;
+
+    public Connection[] getConnections();
+
+}
