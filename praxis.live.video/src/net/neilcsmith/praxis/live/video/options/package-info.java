84495d109f436903b70f9e89c169190317cfb849
Neil C Smith <neilcsmith.net@googlemail.com>
2015-03-09 15:20:16 +0000

    praxis.live.video - move Video options to left of KeyMap.

--- praxis.live.video/src/net/neilcsmith/praxis/live/video/options/package-info.java
+++ praxis.live.video/src/net/neilcsmith/praxis/live/video/options/package-info.java
@@ -1,5 +1,5 @@
 
-@ContainerRegistration(id = "Video", categoryName = "#OptionsCategory_Name_Video", iconBase = "net/neilcsmith/praxis/live/video/resources/video32.png", keywords = "#OptionsCategory_Keywords_Video", keywordsCategory = "Video", position = 1000)
+@ContainerRegistration(id = "Video", categoryName = "#OptionsCategory_Name_Video", iconBase = "net/neilcsmith/praxis/live/video/resources/video32.png", keywords = "#OptionsCategory_Keywords_Video", keywordsCategory = "Video", position = 500)
 package net.neilcsmith.praxis.live.video.options;
 
 import org.netbeans.spi.options.OptionsPanelController.ContainerRegistration;
