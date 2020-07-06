5759c237f544cdb2d4c9b8a20c890573b5267c52
Neil C Smith <neilcsmith.net@googlemail.com>
2020-07-06 15:43:40 +0100

    Set up editor theme; fix property row height; remove no longer needed UI code.

--- praxis.live.laf/src/org/praxislive/ide/laf/Installer.java
+++ praxis.live.laf/src/org/praxislive/ide/laf/Installer.java
@@ -4,76 +4,142 @@
  * Copyright 2020 Neil C Smith.
  *
  * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
+ * under the terms of the GNU General Public License version 3 only, as
  * published by the Free Software Foundation.
  *
  * This code is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details.
+ * version 3 for more details.
  *
- * You should have received a copy of the GNU General Public License version 2
+ * You should have received a copy of the GNU General Public License version 3
  * along with this work; if not, see http://www.gnu.org/licenses/
  *
- *
- * Linking this work statically or dynamically with other modules is making a
- * combined work based on this work. Thus, the terms and conditions of the GNU
- * General Public License cover the whole combination.
- *
- * As a special exception, the copyright holders of this work give you permission
- * to link this work with independent modules to produce an executable,
- * regardless of the license terms of these independent modules, and to copy and
- * distribute the resulting executable under terms of your choice, provided that
- * you also meet, for each linked independent module, the terms and conditions of
- * the license of that module. An independent module is a module which is not
- * derived from or based on this work. If you modify this work, you may extend
- * this exception to your version of the work, but you are not obligated to do so.
- * If you do not wish to do so, delete this exception statement from your version.
  *
  * Please visit http://neilcsmith.net if you need additional information or
  * have any questions.
  */
 package org.praxislive.ide.laf;
 
+import java.lang.reflect.Method;
+import java.util.Collection;
 import java.util.prefs.Preferences;
+import javax.swing.LookAndFeel;
+import javax.swing.UIManager;
 import org.openide.modules.ModuleInstall;
-import org.openide.util.ImageUtilities;
+import org.openide.util.Lookup;
 import org.openide.util.NbPreferences;
-import org.openide.windows.TopComponent;
 import org.openide.windows.WindowManager;
 
 /**
  *
  */
 public class Installer extends ModuleInstall {
+    
+    private static final String COLOR_MODEL_CLASS_NAME =
+            "org.netbeans.modules.options.colors.ColorModel"; //NOI18N
+    
+    private static boolean switchColors = false;
 
     @Override
     public void validate() throws IllegalStateException {
-        Preferences prefs = getPreferences();
-        prefs.put("laf", "com.formdev.flatlaf.FlatDarkLaf");
-    }
-    
-    @Override
-    public void restored() {
-
+        Preferences prefs = NbPreferences.root().node("laf");
+        if (prefs.get("laf", "").isBlank()) {
+            prefs.put("laf", "com.formdev.flatlaf.FlatDarkLaf");
+            switchColors = true;
+        }
+        
         System.setProperty("netbeans.ps.hideSingleExpansion", "true");
         System.setProperty("ps.quickSearch.disabled.global", "true");
-
-        WindowManager.getDefault().invokeWhenUIReady(new Runnable() {
-
-            @Override
-            public void run() {
-                WindowManager wm = WindowManager.getDefault();
-                TopComponent tc = wm.findTopComponent("projectTabLogical_tc");
-                tc.setIcon(ImageUtilities.loadImage("org/netbeans/modules/project/ui/resources/projectTab.png", true));
-                tc = wm.findTopComponent("CommonPalette");
-                tc.setIcon(ImageUtilities.loadImage("org/netbeans/modules/palette/resources/palette.png", true));
-            }
-        });
-
     }
 
-    private Preferences getPreferences() {
-        return NbPreferences.root().node("laf");
+    @Override
+    public void restored() {
+        WindowManager.getDefault().invokeWhenUIReady(this::configureUI);
+        
     }
+    
+    private void configureUI() {
+        var defs = UIManager.getDefaults();
+        var font = defs.getFont("Label.font");
+        if (font != null) {
+            int size = font.getSize();
+            defs.put("netbeans.ps.rowheight", size * 2);
+        }
+        
+        if (switchColors) {
+            switchEditorColorsProfile();
+        }
+    }
+
+    private boolean isChangeEditorColorsPossible() {
+        String preferredProfile = getPreferredColorProfile();
+        ClassLoader loader = Lookup.getDefault().lookup(ClassLoader.class);
+        if (loader == null) {
+            loader = ClassLoader.getSystemClassLoader();
+        }
+        try {
+            Class clazz = loader.loadClass(COLOR_MODEL_CLASS_NAME);
+            Object colorModel = clazz.getDeclaredConstructor().newInstance();
+            Method method = clazz.getDeclaredMethod("getCurrentProfile", new Class[0]);
+            Object invokeResult = method.invoke(colorModel, new Object[0]);
+            return invokeResult != null && !preferredProfile.equals(invokeResult);
+        } catch (Exception ex) {
+            //ignore
+            System.getLogger(Installer.class.getName()).log(System.Logger.Level.INFO,
+                    "Cannot get the current editor colors profile.", ex);
+        }
+        return false;
+    }
+
+    private void switchEditorColorsProfile() {
+        if (!isChangeEditorColorsPossible()) {
+            return;
+        }
+
+        String preferredProfile = getPreferredColorProfile();
+        
+        ClassLoader loader = Lookup.getDefault().lookup(ClassLoader.class);
+        if (loader == null) {
+            loader = ClassLoader.getSystemClassLoader();
+        }
+        try {
+            Class clazz = loader.loadClass(COLOR_MODEL_CLASS_NAME);
+            Object colorModel = clazz.getDeclaredConstructor().newInstance();
+            Method method = clazz.getDeclaredMethod("setCurrentProfile", String.class);
+            method.invoke(colorModel, preferredProfile);
+
+            // method call above changes the token colors but not annotation
+            // colors. these two seems to solve the problem
+            method = clazz.getDeclaredMethod("getAnnotations", String.class);
+            Object acs = method.invoke(colorModel, preferredProfile);
+
+            method = clazz.getDeclaredMethod("setAnnotations", String.class, Collection.class);
+            method.invoke(colorModel, preferredProfile, acs);
+        } catch (Exception ex) {
+            //ignore
+            System.getLogger(Installer.class.getName()).log(System.Logger.Level.INFO,
+                    "Cannot change editors colors profile.", ex);
+        }
+    }
+    
+    private String getPreferredColorProfile() {
+        String className = NbPreferences.root().node( "laf" ).get( "laf", null );
+        if( null == className )
+            return null;
+
+        ClassLoader loader = Lookup.getDefault().lookup( ClassLoader.class );
+        if( null == loader )
+            loader = ClassLoader.getSystemClassLoader();
+
+        try {
+            Class clazz = loader.loadClass( className );
+            LookAndFeel laf = ( LookAndFeel ) clazz.getDeclaredConstructor().newInstance();
+            return laf.getDefaults().getString( "nb.preferred.color.profile" ); //NOI18N
+        } catch( Exception e ) {
+            //ignore
+        }
+        return null;
+    }
+    
 }
