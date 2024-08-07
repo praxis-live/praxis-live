45c6b85ee012ac1518e5c2f772ed51efb3f12a60
Neil C Smith <neilcsmith.net@googlemail.com>
2024-08-07 16:27:23 +0100

    Move to file based PXR root templates.

--- modules/praxislive.pxr/src/main/java/org/praxislive/ide/pxr/PXRWizardIterator.java
+++ modules/praxislive.pxr/src/main/java/org/praxislive/ide/pxr/PXRWizardIterator.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 2020 Neil C Smith.
+ * Copyright 2024 Neil C Smith.
  *
  * This code is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 3 only, as
@@ -22,23 +22,26 @@
 package org.praxislive.ide.pxr;
 
 import java.awt.EventQueue;
-import java.io.File;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
 import java.lang.reflect.InvocationTargetException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.Collections;
 import java.util.NoSuchElementException;
 import java.util.Set;
 import java.util.logging.Logger;
 import javax.swing.event.ChangeListener;
-import org.praxislive.core.ComponentType;
 import org.praxislive.ide.core.api.Callback;
 import org.praxislive.ide.project.api.ExecutionLevel;
 import org.praxislive.ide.project.api.PraxisProject;
 import org.netbeans.api.project.Project;
 import org.netbeans.api.project.ProjectUtils;
+import org.netbeans.api.templates.TemplateRegistration;
+import org.netbeans.api.templates.TemplateRegistrations;
 import org.netbeans.spi.project.ActionProvider;
+import org.netbeans.spi.project.ProjectServiceProvider;
+import org.netbeans.spi.project.ui.PrivilegedTemplates;
 import org.netbeans.spi.project.ui.templates.support.Templates;
 import org.openide.DialogDisplayer;
 import org.openide.NotifyDescriptor;
@@ -50,13 +53,41 @@ import org.openide.util.Exceptions;
 import org.openide.util.Lookup;
 import org.openide.util.NbBundle;
 import org.praxislive.ide.project.api.ProjectProperties;
+import org.praxislive.project.GraphModel;
+import org.praxislive.project.ParseException;
 
+@TemplateRegistrations({
+    @TemplateRegistration(folder = "PXR",
+            position = 100,
+            content = "resources/audio.pxr",
+            displayName = "#TPL_Audio",
+            category = "PXR"),
+    @TemplateRegistration(folder = "PXR",
+            position = 200,
+            content = "resources/video.pxr",
+            displayName = "#TPL_Video",
+            category = "PXR"),
+    @TemplateRegistration(folder = "PXR",
+            position = 300,
+            content = "resources/gui.pxr",
+            displayName = "#TPL_GUI",
+            category = "PXR"),
+    @TemplateRegistration(folder = "PXR",
+            position = 400,
+            content = "resources/root.pxr",
+            displayName = "#TPL_Generic",
+            category = "PXR")
+})
 @NbBundle.Messages({
     "TITLE_buildProject=Build project?",
     "# {0} - project name",
-    "MSG_buildProject=Build project {0}?"
+    "MSG_buildProject=Build project {0}?",
+    "TPL_Audio=Audio patch",
+    "TPL_Video=Video patch",
+    "TPL_GUI=Control Panel (GUI)",
+    "TPL_Generic=Generic data patch"
 })
-public final class PXRWizardIterator implements WizardDescriptor.InstantiatingIterator {
+public final class PXRWizardIterator implements WizardDescriptor.InstantiatingIterator<WizardDescriptor> {
 
     final static Logger LOG = Logger.getLogger(PXRWizardIterator.class.getName());
 
@@ -66,87 +97,34 @@ public final class PXRWizardIterator implements WizardDescriptor.InstantiatingIt
     private int index;
     private WizardDescriptor wizard;
     private WizardDescriptor.Panel[] panels;
-    private ComponentType rootType;
 
-    /**
-     * Initialize panels representing individual wizard's steps and sets various
-     * properties for them influencing wizard appearance.
-     */
-    private WizardDescriptor.Panel[] getPanels() {
-        if (panels == null) {
-            panels = new WizardDescriptor.Panel[]{
-                new PXRWizardPanel1(rootType)
-            };
-
-            // @TODO - this seems to break Templates.getTargetFolder() ???
-//            String[] steps = createSteps();
-//            for (int i = 0; i < panels.length; i++) {
-//                Component c = panels[i].getComponent();
-//                if (steps[i] == null) {
-//                    // Default step name to component name of panel. Mainly
-//                    // useful for getting the name of the target chooser to
-//                    // appear in the list of steps.
-//                    steps[i] = c.getName();
-//                }
-//                if (c instanceof JComponent) { // assume Swing components
-//                    JComponent jc = (JComponent) c;
-//                    // Sets step number of a component
-//                    // TODO if using org.openide.dialogs >= 7.8, can use WizardDescriptor.PROP_*:
-//                    jc.putClientProperty("WizardPanel_contentSelectedIndex", new Integer(i));
-//                    // Sets steps names for a panel
-//                    jc.putClientProperty("WizardPanel_contentData", steps);
-//                    // Turn on subtitle creation on each step
-//                    jc.putClientProperty("WizardPanel_autoWizardStyle", Boolean.TRUE);
-//                    // Show steps on the left side with the image on the background
-//                    jc.putClientProperty("WizardPanel_contentDisplayed", Boolean.TRUE);
-//                    // Turn on numbering of all steps
-//                    jc.putClientProperty("WizardPanel_contentNumbered", Boolean.TRUE);
-//                }
-//            }
-        }
-        return panels;
+    @Override
+    public void initialize(WizardDescriptor wizard) {
+        this.wizard = wizard;
+        FileObject projectDir = Templates.getProject(wizard).getProjectDirectory();
+        FileObject templateFile = Templates.getTemplate(wizard);
+        panels = new WizardDescriptor.Panel[]{
+            new PXRWizardPanel1(projectDir, templateFile)
+        };
     }
 
     @Override
     public Set instantiate() throws IOException {
-        Object obj = wizard.getProperty(PROP_PXR_ID);
-        String id;
-        if (obj instanceof String) {
-            id = (String) obj;
-        } else {
-            throw new IOException("No id in wizard");
-        }
-        obj = wizard.getProperty(PROP_PXR_FILE);
-        File file;
-        if (obj instanceof File) {
-            file = (File) obj;
-        } else {
-            throw new IOException("No file in wizard");
-        }
-        if (file.exists()) {
-            throw new IOException("File already exists");
-        }
-        obj = wizard.getProperty(PROP_PXR_TYPE);
-        ComponentType type;
-        if (obj instanceof ComponentType) {
-            type = (ComponentType) obj;
-        } else {
-            throw new IOException("No type in wizard");
-        }
-        boolean build = true;
-        boolean autostart = true;
+        try {
+            String id = wizard.getProperty(PROP_PXR_ID).toString();
+            Project project = Templates.getProject(wizard);
+            FileObject projectDir = project.getProjectDirectory();
+            FileObject templateFile = Templates.getTemplate(wizard);
 
-        FileObject fileObj = FileUtil.createData(file);
-        writeFile(fileObj, id, type, autostart);
-
-        Project project = Templates.getProject(wizard);
-        if (project != null) {
+            FileObject file = createFile(projectDir, templateFile, id);
+            boolean build = true;
+            boolean autostart = true;
 
             Runnable task = () -> {
                 ProjectProperties props = project.getLookup().lookup(ProjectProperties.class);
                 if (props != null) {
                     try {
-                        props.addFile(ExecutionLevel.BUILD, fileObj);
+                        props.addFile(ExecutionLevel.BUILD, file);
                         if (autostart) {
                             props.addLine(ExecutionLevel.RUN, "/" + id + ".start");
                         }
@@ -157,10 +135,10 @@ public final class PXRWizardIterator implements WizardDescriptor.InstantiatingIt
                 }
 
                 if (build) {
-                    buildFile(project, fileObj);
+                    buildFile(project, file);
                 }
             };
-            
+
             if (EventQueue.isDispatchThread()) {
                 task.run();
             } else {
@@ -169,75 +147,74 @@ public final class PXRWizardIterator implements WizardDescriptor.InstantiatingIt
                     // need to invoke and wait or dialog is closed and project
                     // built automatically when wizard closes.
                     EventQueue.invokeAndWait(task);
-                } catch (InterruptedException ex) {
-                    Exceptions.printStackTrace(ex);
-                } catch (InvocationTargetException ex) {
+                } catch (InterruptedException | InvocationTargetException ex) {
                     Exceptions.printStackTrace(ex);
                 }
             }
 
-        } else {
-            LOG.warning("No project found for wizard");
-        }
-
-        return Collections.singleton(fileObj);
-    }
-
-    private void writeFile(FileObject file, String id, ComponentType type, boolean autostart) throws IOException {
-        CharSequence code;
-        if ("root:video".equals(type.toString())) {
-            code = getVideoFileTemplate(id);
-        } else if ("root:audio".equals(type.toString())) {
-            code = getAudioFileTemplate(id);
-        } else {
-            code = getDefaultFileTemplate(id, type);
-        }
-        Writer writer = null;
-        try {
-            writer = new OutputStreamWriter(file.getOutputStream());
-            writer.append(code);
-        } finally {
-            if (writer != null) {
-                writer.close();
-            }
+            return Collections.singleton(file);
+        } catch (ParseException ex) {
+            throw new IOException(ex);
         }
     }
 
-    private CharSequence getDefaultFileTemplate(String id, ComponentType type) {
-        StringBuilder code = new StringBuilder();
-        code.append("@ /").append(id).append(" ").append(type).append(" {\n");
-        code.append("}");
-        return code;
+    @Override
+    public void uninitialize(WizardDescriptor wizard) {
+        panels = null;
     }
 
-    private CharSequence getVideoFileTemplate(String id) {
-        String template = "@ /video root:video {\n"
-                + "  #%pxr.format 4\n"
-                + "  #%autostart true\n"
-                + "  .width 800\n"
-                + "  .height 600\n"
-                + "  .fps 60.0\n"
-                + "  .renderer OpenGL\n"
-                + "  @ ./screen video:output {\n"
-                + "    #%graph.x 600\n"
-                + "    #%graph.y 200\n"
-                + "    .always-on-top true\n"
-                + "    .show-cursor true\n"
-                + "  }\n"
-                + "}";
-        return template.replace("/video", "/" + id);
+    @Override
+    public WizardDescriptor.Panel current() {
+        return panels[index];
     }
 
-    private CharSequence getAudioFileTemplate(String id) {
-        String template = "@ /audio root:audio {\n"
-                + "  #%pxr.format 4\n"
-                + "  #%autostart true\n"
-                + "  @ ./output audio:output {\n"
-                + "    #%graph.x 600\n"
-                + "    #%graph.y 200\n"
-                + "  }\n"
-                + "}";
-        return template.replace("/audio", "/" + id);
+    @Override
+    public String name() {
+        return index + 1 + ". from " + panels.length;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return index < panels.length - 1;
+    }
+
+    @Override
+    public boolean hasPrevious() {
+        return index > 0;
+    }
+
+    @Override
+    public void nextPanel() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        index++;
+    }
+
+    @Override
+    public void previousPanel() {
+        if (!hasPrevious()) {
+            throw new NoSuchElementException();
+        }
+        index--;
+    }
+
+    @Override
+    public void addChangeListener(ChangeListener l) {
+    }
+
+    @Override
+    public void removeChangeListener(ChangeListener l) {
+    }
+
+    private FileObject createFile(FileObject projectDir, FileObject templateFile, String id) throws IOException, ParseException {
+        String script = templateFile.asText();
+        GraphModel model = GraphModel.parse(script);
+        model = model.withRename(id);
+        script = model.writeToString();
+        Path file = FileUtil.toPath(projectDir).resolve(id + ".pxr");
+        Files.writeString(file, script, StandardOpenOption.CREATE_NEW);
+        return FileUtil.toFileObject(file);
     }
 
     private void buildFile(Project base, FileObject file) {
@@ -269,119 +246,19 @@ public final class PXRWizardIterator implements WizardDescriptor.InstantiatingIt
         }
     }
 
-    @Override
-    public void initialize(WizardDescriptor wizard) {
-        this.wizard = wizard;
-        FileObject template = Templates.getTemplate(wizard);
-        Object typeAttr = template.getAttribute("rootType");
-        if (typeAttr != null) {
-            try {
-                rootType = ComponentType.of(typeAttr.toString());
-            } catch (Exception ex) {
-                // do nothing
-            }
-        }
-    }
+    @ProjectServiceProvider(service = PrivilegedTemplates.class, projectType = "org-praxislive-ide-project")
+    public static class PrivilegedTemplatesImpl implements PrivilegedTemplates {
 
-    @Override
-    public void uninitialize(WizardDescriptor wizard) {
-        panels = null;
-    }
-
-    @Override
-    public WizardDescriptor.Panel current() {
-        return getPanels()[index];
-    }
-
-    @Override
-    public String name() {
-        return index + 1 + ". from " + getPanels().length;
-    }
-
-    @Override
-    public boolean hasNext() {
-        return index < getPanels().length - 1;
-    }
-
-    @Override
-    public boolean hasPrevious() {
-        return index > 0;
-    }
-
-    @Override
-    public void nextPanel() {
-        if (!hasNext()) {
-            throw new NoSuchElementException();
-        }
-        index++;
-    }
-
-    @Override
-    public void previousPanel() {
-        if (!hasPrevious()) {
-            throw new NoSuchElementException();
-        }
-        index--;
-    }
-
-    // If nothing unusual changes in the middle of the wizard, simply:
-    @Override
-    public void addChangeListener(ChangeListener l) {
-    }
-
-    @Override
-    public void removeChangeListener(ChangeListener l) {
-    }
-
-    // If something changes dynamically (besides moving between panels), e.g.
-    // the number of panels changes in response to user input, then uncomment
-    // the following and call when needed: fireChangeEvent();
-    /*
-    private Set<ChangeListener> listeners = new HashSet<ChangeListener>(1); // or can use ChangeSupport in NB 6.0
-    public final void addChangeListener(ChangeListener l) {
-    synchronized (listeners) {
-    listeners.add(l);
-    }
-    }
-    public final void removeChangeListener(ChangeListener l) {
-    synchronized (listeners) {
-    listeners.remove(l);
-    }
-    }
-    protected final void fireChangeEvent() {
-    Iterator<ChangeListener> it;
-    synchronized (listeners) {
-    it = new HashSet<ChangeListener>(listeners).iterator();
-    }
-    ChangeEvent ev = new ChangeEvent(this);
-    while (it.hasNext()) {
-    it.next().stateChanged(ev);
-    }
-    }
-     */
-    // You could safely ignore this method. Is is here to keep steps which were
-    // there before this wizard was instantiated. It should be better handled
-    // by NetBeans Wizard API itself rather than needed to be implemented by a
-    // client code.
-    private String[] createSteps() {
-        String[] beforeSteps = null;
-        Object prop = wizard.getProperty("WizardPanel_contentData");
-        if (prop != null && prop instanceof String[]) {
-            beforeSteps = (String[]) prop;
+        @Override
+        public String[] getPrivilegedTemplates() {
+            return new String[]{
+                "Templates/PXR/audio.pxr",
+                "Templates/PXR/video.pxr",
+                "Templates/PXR/gui.pxr",
+                "Templates/PXR/root.pxr"
+            };
         }
 
-        if (beforeSteps == null) {
-            beforeSteps = new String[0];
-        }
-
-        String[] res = new String[(beforeSteps.length - 1) + panels.length];
-        for (int i = 0; i < res.length; i++) {
-            if (i < (beforeSteps.length - 1)) {
-                res[i] = beforeSteps[i];
-            } else {
-                res[i] = panels[i - beforeSteps.length + 1].getComponent().getName();
-            }
-        }
-        return res;
     }
+
 }
